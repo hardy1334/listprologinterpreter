d89db84e006c85c137a4895df0f361fadf32baa3
Lucian Green <luciangreen@lucianacademy.com>
2019-06-06 20:19:21 +1000

    Add files via upload

--- meditatorsanddoctors.pl
+++ meditatorsanddoctors.pl
@@ -4,10 +4,80 @@
 
 meditators([
 %%/**
-[first,last,dobd,dobm,doby,learntd,learntm,learnty,1,0,16], %% 
-[first,last,dobd,dobm,doby,learntd,learntm,learnty,1,0,16] %% 
+[lucian,green,8,8,1980,23,9,2018,1,0,16], %% Established LM in 2013
+[adrian,schonfelder,18,12,1974,22,9,2018,1,1,16], %% Learned 8,6,2015
+
+[allan,powell,26,1,2019,25,9,2018,1,3,16],
+[brae,schonfelder,1,1,2018,14,10,2018,1,4,16],
+[skye,schonfelder,23,6,2007,14,10,2018,1,5,16],
+[whistler,schonfelder,13,4,2018,30,12,2018,1,6,16], %% Stop kill ideation
+%% 5
+%% [soft,schonfelder,14,2,2019,1,1,2018], %% Stop suicidal ideation
+[dixie,schonfelder,17,2,2019,1,1,2018,1,7,16],
+[wilbur,schonfelder,17,2,2019,1,1,2018,1,8,16],
+[duke,schonfelder,17,2,2019,1,1,2018,1,9,16],
+%%**/
+
+[leonardodavinci,schonfelder,17,2,2019,1,1,2018,1,10,16],
+[venice,schonfelder,17,2,2019,1,1,2018,1,11,16],
+
+[sydney,schonfelder,17,2,2019,1,1,2018,1,12,16],
+[donald,schonfelder,17,2,2019,1,1,2018,1,13,16],
+[countess,schonfelder,17,2,2019,1,1,2018,1,14,16],
+[blackie,schonfelder,17,2,2019,1,1,2018,1,15,16],
+[blackduck,schonfelder,17,2,2019,1,1,2018,1,16,16],
+[greyhen,schonfelder,17,2,2019,1,1,2018,1,17,16],
+[blackandwhitehen,schonfelder,17,2,2019,1,1,2018,1,18,16],
+[blackandwhitehenwithblackspeckles,schonfelder,17,2,2019,1,1,2018,1,19,16],
+[whiterhen,schonfelder,17,2,2019,1,1,2018,1,20,16],
+[neil,batt,27,4,2019,1,1,0001,1,23,16]
+
 ]).
 
-%% empty, because meditators is now based on 50 As
 doctors([
+%%/**
+
+%%19?, Total=21
+%%**/
+
 ]).
+
+/**
+Taken off 20 10 18
+[inky,green,1,5,2004,9,10,2018]
+
+Taken off 16 10 18
+[brae,schonfelder,1,1,2018,14,10,2018],
+[skye,schonfelder,23,6,2007,14,10,2018]
+
+Put on 30 12 18
+[brae,schonfelder,1,1,2018,14,10,2018],
+[skye,schonfelder,23,6,2007,14,10,2018],
+[whistler,schonfelder,13,4,2018,30,12,2018]
+
+allan's year of birth unknown 26 1 19
+
+stopped using this list on 23 3 19
+
+started using this list on 19 4 19
+minus soft
+but actually kept on using it, minus soft
+
+27 4 19
+Add John from Barrabool, Brian Porter and Neil Batt to doctors list.
+
+[secondhen,schonfelder,17,2,2019,1,1,2018],
+- miscounted? removed
+
+9 5 19 removed
+[john,frombarrabool,27,4,2019,1,1,0001,1,21,16],
+[brian,porter,27,4,2019,1,1,0001,1,22,16],
+[neil,oliver,2,5,2019,1,1,0001,1,24,16]
+
+14 5 19
+with addition of arem, friendliness to meditation, moved doctors to meditators to use the new utterances
+commented out adrian and others because they are not real meditators
+** give adrian medicine course every 2 years instead
+with graciously give or blame + radio button, keep adrian et al.
+**/
+
